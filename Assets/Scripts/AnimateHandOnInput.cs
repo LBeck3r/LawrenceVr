3eedf44e7d49104c21d3deff5f5ad2d25e66fe16
Larry <51520116+LBeck3r@users.noreply.github.com>
2022-11-26 17:27:36 -0500

    Add Oculus hands and animations

---
+++ Assets/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+
+    public Animator handAnimator;
+
+    void Update()
+    {
+        var triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        var gripValue = gripAnimationAction.action.ReadValue<float>();
+
+        handAnimator.SetFloat("Trigger", triggerValue);
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
