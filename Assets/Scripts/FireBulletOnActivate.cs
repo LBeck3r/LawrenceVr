a292305df5508d3fb1227d422f24eeb255d38e63
Larry <51520116+LBeck3r@users.noreply.github.com>
2022-11-26 19:56:31 -0500

    Add fireable gun

---
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float bulletSpeed = 25;
+
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    public void FireBullet(ActivateEventArgs args)
+    {
+        GameObject firedBullet = Instantiate(bullet);
+
+        firedBullet.transform.position = spawnPoint.position;
+        firedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * bulletSpeed;
+
+        Destroy(firedBullet, 5);
+    }
+}
